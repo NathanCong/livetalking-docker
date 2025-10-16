bff894053e63e76edefdd9f900012f79ea0764fb
NathanCong <congyunan@126.com>
2025-10-16 13:12:50 +0800

    feat: 更新 start.sh 脚本

--- volumes/scripts/start.sh
+++ volumes/scripts/start.sh
@@ -3,7 +3,22 @@
 # 初始化 miniconda
 source /root/miniconda3/etc/profile.d/conda.sh
 
+# 进入项目目录
+cd /root/LiveTalking
+
+# 激活环境
+conda activate nerfstream
+
+# 设置环境变量
+export DOUBAO_APPID=3357549641
+export DOUBAO_TOKEN=7wSj2mnvz6aFibl2jDOdREFYf_NnW2wX
+
 # 启动服务
-cd /root/LiveTalking && \
-conda activate nerfstream && \
-python app.py --transport webrtc --model musetalk --avatar_id avator_3 --max_session 10 --REF_FILE zh-CN-XiaoxiaoNeural --listenport 8011
+python app.py \
+--transport webrtc \
+--model musetalk \
+--avatar_id avator_3 \
+--max_session 5 \
+--tts doubao \
+--REF_FILE zh_female_qingxinnvsheng_mars_bigtts \
+--listenport 8011
